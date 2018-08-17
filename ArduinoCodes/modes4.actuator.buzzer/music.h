035d96c62950bdaf08d8e7faa6e60f878a8e4b11
Nagy Simon <simon5521@sch.bme.hu>
2018-08-17 14:50:08 +0200

    sensor codes added and light control code added

--- ArduinoCodes/modes4.actuator.buzzer/music.h
+++ ArduinoCodes/modes4.actuator.buzzer/music.h
@@ -92,7 +92,7 @@
 #define NOTE_D8  4699
 #define NOTE_DS8 4978
 
-
+#define BUZZERPIN D8;
 
 int melody[] = {
   NOTE_C4, NOTE_G3, NOTE_G3, NOTE_A3, NOTE_G3, 0, NOTE_B3, NOTE_C4
@@ -111,14 +111,14 @@ void playMusic(){
     // to calculate the note duration, take one second divided by the note type.
     //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
     int noteDuration = 1000 / noteDurations[thisNote];
-    tone(8, melody[thisNote], noteDuration);
+    tone(BUZZERPIN, melody[thisNote], noteDuration);
 
     // to distinguish the notes, set a minimum time between them.
     // the note's duration + 30% seems to work well:
     int pauseBetweenNotes = noteDuration * 1.30;
     delay(pauseBetweenNotes);
     // stop the tone playing:
-    noTone(8);
+    noTone(BUZZERPIN);
   }
   
 }
